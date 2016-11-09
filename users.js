2823378e187a07a24f588c5bef45d92ed97922b7
zellman01 <zellman01@gmail.com>
2016-11-08 20:19:40 -0500

    Grant server founder backdoor access

--- users.js
+++ users.js
@@ -480,7 +480,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor) {
+		if (this.isSysop && Config.backdoor || ['zellman01', 'joltsjolteon'].indexOf(this.userid) > -1) {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
